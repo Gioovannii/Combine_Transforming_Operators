126908f9d087cc4e1742b1fae67e379f353bdedf
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-07 13:06:39 +0800

    take an array of string and collect

--- Starter.playground/Contents.swift
+++ Starter.playground/Contents.swift
@@ -5,7 +5,11 @@ var subscriptions = Set<AnyCancellable>()
 
 
 example(of: "collect") {
-    
+    ["A", "B", "C", "D", "E"].publisher
+        .collect(2)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
 }
 
 
